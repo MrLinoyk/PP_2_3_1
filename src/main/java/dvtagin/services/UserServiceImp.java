862d528fbfa104124361e88faff96794b4369804
Dmitriy <freejase@bk.ru>
2022-05-04 22:04:55 +0300

    @Transactional только для метода remove(), так как он получает данные по id, и для него требуется единое обращение к БД.

--- src/main/java/dvtagin/services/UserServiceImp.java
+++ src/main/java/dvtagin/services/UserServiceImp.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 public class UserServiceImp implements UserService{
 
     private UserDao userDao;
@@ -36,6 +35,7 @@ public class UserServiceImp implements UserService{
     }
 
     @Override
+    @Transactional
     public void removeUser(int id) {
         userDao.removeUser(id);
     }
